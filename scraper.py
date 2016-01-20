77054eec747dcaa1a990616ab2a750ca7041eac0
mrmodiddley <mrmodiddley@gmail.com>
2016-01-20 16:30:22 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ print urltoscrape
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
-My age = 180
+My_age = 180
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
 
