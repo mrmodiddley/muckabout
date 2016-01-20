0070b187003ea94cda0941a7f717fa758b69e8de
mrmodiddley <mrmodiddley@gmail.com>
2016-01-20 16:18:20 +0000

    added intro

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
 #
+print 'hello world, I'm a happy but tired'
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
